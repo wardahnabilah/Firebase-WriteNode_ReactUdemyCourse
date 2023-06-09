a5fa75d1f3b914537961a99fe28f9e43dafdc88b
unknown <wardahnabilah33@gmail.com>
2023-06-09 07:28:19 +0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="container">
+      <AllRoutes />
+    </div>
   );
 }
 
