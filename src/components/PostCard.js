030cce21d2fee10eb9116cdcd83b8a459dc29a79
unknown <wardahnabilah33@gmail.com>
2023-06-11 20:25:09 +0700

    Create 'Delete Post' functionality

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./PostCard.css"
+import { deleteDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase/config'
 
 export function PostCard({ id, title, description, createdBy }) {
+  
+  async function handleDelete() {
+    const post = doc(db, "posts", id)
+
+    deleteDoc(post)
+  }
+  
   return (
     <div className="postcard-container">
-        <h3 className="title">{title}</h3>
+        <div className="title">
+          <h3>{title}</h3>
+          <i onClick={handleDelete} className="bi bi-trash-fill delete"></i>
+        </div>
         <p className="content">{description}</p>
         <div className="createdby">Created by: {createdBy.userName}</div>
     </div>
