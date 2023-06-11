7e6690379be0a141ebb609c861bfe8eaf7d58837
unknown <wardahnabilah33@gmail.com>
2023-06-11 20:15:37 +0700

    Create 'Add new post' functionality and store it in firebase

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { collection, addDoc } from 'firebase/firestore'
+import { auth, db } from '../firebase/config'
 import './CreatePost.css'
 
 export function CreatePost() {
+  const colRef = collection(db, "posts")
+  const navigate = useNavigate()
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const title = event.target.title.value
+    const desc = event.target.description.value
+
+    await addDoc(colRef, {
+      title: title,
+      description: desc,
+      createdBy: {
+        userId: auth.currentUser.uid,
+        userName: auth.currentUser.displayName
+      }
+    })
+
+    navigate("/")
+  }
+  
   return (
     <main className="container">
         <h1 className="create-heading">Add New Post</h1>
-        <form className="create-form">
+        <form onSubmit={handleSubmit} className="create-form">
             <input type="text" name="title" placeholder="Title"/>
             <textarea name="description" placeholder="Description" cols="30" rows="10"></textarea>
             <button className="btn-primary">Create</button>
