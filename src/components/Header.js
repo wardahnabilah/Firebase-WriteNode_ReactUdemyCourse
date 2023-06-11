27ad78e4785a0027593899ae8bd4c20b510d37e2
unknown <wardahnabilah33@gmail.com>
2023-06-11 19:37:29 +0700

    Create Login and Logout functionality using Firebase Authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../assets/images/logo.png'
 import "./Header.css"
+import { signInWithPopup, signOut } from 'firebase/auth'
+import { auth, provider } from '../firebase/config'
 
 export function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState(JSON.parse(localStorage.getItem("isLoggedIn")) || false)
+
+  function handleLogin() {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      localStorage.setItem("isLoggedIn", true)
+      setIsLoggedIn(true)
+    })
+  }
+
+  function handleLogout() {
+    signOut(auth)
+    localStorage.setItem("isLoggedIn", false)
+    setIsLoggedIn(false)
+  }
+  
   return (
     <header className="navbar">
         <Link to="/" className="logo">
@@ -12,9 +31,12 @@ export function Header() {
         </Link>
         <nav className="nav">
             <NavLink to="/" className="nav-item active" end>Home</NavLink>
-            {/* <NavLink to="/create" className="nav-item">Create Post</NavLink> */}
-            <button className="nav-item btn"><i className="bi bi-google"></i>Login</button>
-            {/* <button className="nav-item btn">Logout</button> */}
+            { isLoggedIn ? (<>
+                              <NavLink to="/create" className="nav-item">Create Post</NavLink>
+                              <button onClick={handleLogout} className="nav-item btn">Logout</button>
+                            </>) :
+                            <button onClick={handleLogin} className="nav-item btn"><i className="bi bi-google"></i>Login</button>
+            }
         </nav>
     </header>
   )
